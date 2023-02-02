629112eddee7f625a05b739e7c964ed84e0ef381
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-02-02 21:50:14 +0000

    Created prototype for link finding function.

--- fark_headline_scraper/web_access.py
+++ fark_headline_scraper/web_access.py
@@ -4,3 +4,16 @@ Web Access Functions
 Functions to access headline data on the website and download it in a raw
 format.
 """
+
+
+def findall_ahref(website_url: str) -> list[str]:
+    """
+    Find all the links on a webpage.
+
+    :param website_url: The url of the webpage to search.
+    :type website_url: str
+    :return: a list of the full urls of the discovered links
+    rtype: list[str]
+    """
+    pass
+
