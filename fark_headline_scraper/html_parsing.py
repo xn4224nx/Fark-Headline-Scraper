f43a2857056d91938b621a51a077b6c215bdab0b
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-02-04 00:43:10 +0000

    Crude prototype of headline extraction.

--- fark_headline_scraper/html_parsing.py
+++ fark_headline_scraper/html_parsing.py
@@ -6,7 +6,7 @@ format.
 """
 
 from bs4 import BeautifulSoup
-
+import re
 
 def findall_href(webpage_text: str) -> list[str]:
     """
@@ -34,4 +34,23 @@ def extract_headline_row(webpage_text: str) -> dict:
     :return: structured metadata of all the headlines in the webpage text
     rtype: dict
     """
-    pass
+    soup = BeautifulSoup(webpage_text, 'html.parser')
+
+    regex = re.compile('.*headlineRow.*')
+
+    # Loop over all the headline rows
+    for headline in soup.find_all("tr", {"class" : regex}):
+
+        # Extract the topic
+        print(headline.find("a", attrs={"title": True})["title"])
+
+        # Extract the URL
+        print(headline.find("a", attrs={"href": True})["href"])
+
+        # Extract the number of comments
+        print(headline.find(class_='headlineComments').text.strip())
+
+        # Extract the headline text
+        print(headline.find("span", class_="headline").text.strip())
+
+        break
