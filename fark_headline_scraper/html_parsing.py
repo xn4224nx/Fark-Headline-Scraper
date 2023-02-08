693f67f4d726ebbcf5893593f4bc2a265984c8cf
xn4224nx <benjamin.dafydd.mouncer@gmail.com>
2023-02-08 21:32:01 +0000

    Modified to better handle errors and only process archive links.

--- fark_headline_scraper/html_parsing.py
+++ fark_headline_scraper/html_parsing.py
@@ -44,8 +44,12 @@ def findall_href(webpage_text: str, page_url: str) -> list[str]:
         if re.match(r"\d{4}-\d{2}-\d{2}", urls[i]):
             ret_ls.append("https://www.fark.com/archives/" + urls[i])
 
-        elif base_url in urls[i]:
-            ret_ls.append(urls[i])
+        # # Remove Fark goto links
+        # if "/goto/" in urls[i]:
+        #     continue
+        #
+        # elif base_url in urls[i]:
+        #     ret_ls.append(urls[i])
 
     return ret_ls
 
@@ -55,7 +59,7 @@ def extract_headline_row(webpage_text: str) -> dict:
     Extract all the headline rows from the webpage text of a Fark webpage and
     return a dict of all the headline metadata.
 
-    :param webpage:
+    :param webpage_text:
     :type webpage_text: str
     :return: structured metadata of all the headlines in the webpage text
     rtype: dict
@@ -69,19 +73,25 @@ def extract_headline_row(webpage_text: str) -> dict:
     # Loop over all the headline rows
     for headline in soup.find_all("tr", {"class": regex}):
 
-        # Extract the article link
-        headline_link = headline.find("a", attrs={"href": True})["href"]
+        try:
+            # Extract the article link
+            headline_link = headline.find("a", attrs={"href": True})["href"]
 
-        # Extract details from the link
-        link_id = re.search(r"goto/(\d+)/", headline_link).group(1)
-        article_root = re.search(r"/\d+/([0-9a-zA-Z\.]+)",
-                                 headline_link).group(1)
+            # Extract details from the link
+            link_id = re.search(r"goto/(\d+)/", headline_link).group(1)
+            article_root = re.search(r"/\d+/([0-9a-zA-Z\.]+)",
+                                     headline_link).group(1)
 
-        headline_data[link_id] = {
-            "Article Site": article_root,
-            "Topic": headline.find("a", attrs={"title": True})["title"],
-            "Comments": headline.find(class_='headlineComments').text.strip(),
-            "Headline": headline.find("span", class_="headline").text.strip()
-        }
+            headline_data[link_id] = {
+                "Article Site": article_root,
+                "Topic": headline.find("a", attrs={"title": True})["title"],
+                "Comments": headline.find(class_='headlineComments'
+                                          ).text.strip(),
+                "Headline": headline.find("span", class_="headline"
+                                          ).text.strip()
+            }
+
+        except AttributeError:
+            pass
 
     return headline_data
